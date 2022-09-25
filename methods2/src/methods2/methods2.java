1ffbd19c513a87b93af4164a51119c01e6d22bf3
Enes <75938150+justedu16@users.noreply.github.com>
2022-09-25 21:13:47 +0300

    Notlar

---
+++ methods2/src/methods2/methods2.java
@@ -0,0 +1,17 @@
+package methods2;
+
+public class methods2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sayi = topla(48,98);
+		System.out.println(sayi);
+
+	}
+	
+	public static int topla(int sayi1, int sayi2) {
+		return sayi1 + sayi2;
+		
+	}
+
+}
