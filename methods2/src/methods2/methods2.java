3762024a347fd0c62e4354fb7534dc4c58eefe38
Enes <75938150+justedu16@users.noreply.github.com>
2022-09-26 16:16:58 +0300

    Notlar

--- methods2/src/methods2/methods2.java
+++ methods2/src/methods2/methods2.java
@@ -6,12 +6,22 @@ public class methods2 {
 		// TODO Auto-generated method stub
 		int sayi = topla(48,98);
 		System.out.println(sayi);
+		int toplam = topla2(2,5,2,5,7,4,3,2,66);
+		System.out.println(toplam);
 
 	}
 	
 	public static int topla(int sayi1, int sayi2) {
 		return sayi1 + sayi2;
 		
+	}
+	public static int topla2(int... sayilar) {
+		int toplam =0;
+		for (int sayi:sayilar) {
+			
+			toplam+=sayi;
+		}
+		return toplam;
 	}
 
 }
