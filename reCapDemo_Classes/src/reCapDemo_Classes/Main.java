f88309264e77b972c8a24744b0c723befb998c8d
Enes <75938150+justedu16@users.noreply.github.com>
2022-09-26 16:28:42 +0300

    Notlar

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,13 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// HESAP MAKİNESİ
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Topla(6, 7);
+		System.out.println(sonuc);
+
+	}
+
+}
